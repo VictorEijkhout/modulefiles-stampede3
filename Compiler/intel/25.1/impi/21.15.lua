b7ed6939ed06ddd388545b9451375e3859a03bf8
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-05-12 12:36:01 -0500

    i25.1

---
+++ Compiler/intel/25.1/impi/21.15.lua
@@ -0,0 +1,12 @@
+-- Compiler/intel/25.1/impi/21.15.lua
+
+inherit()
+
+local version = "21.15"
+
+prepend_path("MODULEPATH",
+    pathJoin(os.getenv("VICTOR_MODULEPATH_ROOT"),
+        "MPI",os.getenv("TACC_FAMILY_COMPILER"),os.getenv("TACC_FAMILY_COMPILER_VERSION"),
+	"impi",version))
+
+family("mpi")
