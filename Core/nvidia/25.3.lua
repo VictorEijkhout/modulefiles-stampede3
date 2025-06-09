2cae45c81f7f5b15be3a07cf1cdf259464494d41
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-06-09 12:40:21 -0500

    we have nv on s3

---
+++ Core/nvidia/25.3.lua
@@ -0,0 +1,12 @@
+inherit()
+
+local compiler = "nvcc"
+local MP_ROOT  = os.getenv("VICTOR_MODULEPATH_ROOT")
+local version  = "25.3"
+
+setenv( "TACC_CC", "nvcc" )
+setenv( "TACC_CXX", "nvc++" )
+setenv( "TACC_FC", "nvfortran" )
+
+prepend_path("MODULEPATH", pathJoin(MP_ROOT, "Compiler",compiler,version))
+family("compiler")
