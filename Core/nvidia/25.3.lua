7c66147093e58c1eff039d4a68b0a36b4cee159a
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-08-21 10:20:32 -0500

    nv fix

--- Core/nvidia/25.3.lua
+++ Core/nvidia/25.3.lua
@@ -4,10 +4,6 @@ local compiler = "nvcc"
 local MP_ROOT  = os.getenv("VICTOR_MODULEPATH_ROOT")
 local version  = "25.3"
 
-setenv( "TACC_CC",  "nvcc" )
-setenv( "TACC_CXX", "nvc++" )
-setenv( "TACC_FC",  "nvfortran" )
-
 prepend_path( "MODULEPATH", pathJoin(MP_ROOT, "Compiler","nvidia",version) )
 
 family("compiler")
