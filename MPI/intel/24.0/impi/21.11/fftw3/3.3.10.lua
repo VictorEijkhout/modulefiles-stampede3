5d7cd46525f6814a98bc0398f820d5a9d2c1abc1
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-05-20 08:27:28 -0500

    rm non-existing mpi

--- MPI/intel/24.0/impi/21.11/fftw3/3.3.10.lua
+++ MPI/intel/24.0/impi/21.11/fftw3/3.3.10.lua
@@ -1,27 +1,31 @@
 local helpMsg = [[
-..
 Package: fftw3/3.3.10
-..
+
+Fastest Fourier Transform in the West, version 3
+Homepage: http://fftw.org/
+
 The fftw3 modulefile defines the following variables:
-TACC_FFTW3_DIR, TACC_FFTW3_LIB, TACC_FFTW3_INC
-..
-http://fftw.org/
-http://www.fftw.org/fftw3_doc/Installation-on-Unix.html
-..
-(modulefile generated Wed Aug  7 11:03:28 AM CDT 2024)
+    TACC_FFTW3_DIR, TACC_FFTW3_LIB, TACC_FFTW3_INC.
+Discoverable by CMake through find_package.
+Discoverable by CMake through pkg-config.
+
+(modulefile generated 2026-02-27)
 ]]
-help(helpMsg)
+
+whatis( "Name:",   "fftw3" )
+whatis( "Version", "3.3.10" )
+
+local prefixdir = "/scratch/00434/eijkhout/installation/installation-fftw3-3.3.10-stampede3-intel24.0-impi21.11"
 setenv( "TACC_FFTW3_VERSION", "3.3.10" )
-setenv( "TACC_FFTW3_DIR",     "/work2/00434/eijkhout/fftw3/installation-fftw3-3.3.10-stampede3-intel24-impi21.11" )
-setenv( "LMOD_FFTW3_DIR",     "/work2/00434/eijkhout/fftw3/installation-fftw3-3.3.10-stampede3-intel24-impi21.11" )
-setenv( "TACC_FFTW3_INC",     pathJoin("/work2/00434/eijkhout/fftw3/installation-fftw3-3.3.10-stampede3-intel24-impi21.11","include")   )
-setenv( "LMOD_FFTW3_INC",     pathJoin("/work2/00434/eijkhout/fftw3/installation-fftw3-3.3.10-stampede3-intel24-impi21.11","include")   )
-setenv( "TACC_FFTW3_LIB",     pathJoin("/work2/00434/eijkhout/fftw3/installation-fftw3-3.3.10-stampede3-intel24-impi21.11","lib")   )
-setenv( "LMOD_FFTW3_LIB",     pathJoin("/work2/00434/eijkhout/fftw3/installation-fftw3-3.3.10-stampede3-intel24-impi21.11","lib")   )
-prepend_path( "LD_LIBRARY_PATH",pathJoin("/work2/00434/eijkhout/fftw3/installation-fftw3-3.3.10-stampede3-intel24-impi21.11","lib")   )
-prepend_path( "PKG_CONFIG_PATH", 
-                  pathJoin( "/work2/00434/eijkhout/fftw3/installation-fftw3-3.3.10-stampede3-intel24-impi21.11","lib","pkgconfig" ) )
-prepend_path( "CMAKE_PREFIX_PATH", 
-                              "/work2/00434/eijkhout/fftw3/installation-fftw3-3.3.10-stampede3-intel24-impi21.11" 
-                            )
--- end of modulefile
+setenv( "LMOD_FFTW3_VERSION", "3.3.10" )
+setenv( "TACC_FFTW3_DIR", prefixdir )
+setenv( "LMOD_FFTW3_DIR", prefixdir )
+setenv( "TACC_FFTW3_INC", pathJoin( prefixdir,"include" ) )
+setenv( "LMOD_FFTW3_INC", pathJoin( prefixdir,"include" ) )
+setenv( "TACC_FFTW3_LIB", pathJoin( prefixdir,"lib" ) )
+setenv( "LMOD_FFTW3_LIB", pathJoin( prefixdir,"lib" ) )
+
+prepend_path( "INCLUDE", pathJoin( prefixdir, "include" ) )
+prepend_path( "LD_LIBRARY_PATH", pathJoin( prefixdir, "lib" ) )
+prepend_path( "PKG_CONFIG_PATH", pathJoin( prefixdir,"lib/pkgconfig" ) )
+prepend_path( "CMAKE_PREFIX_PATH", prefixdir )
