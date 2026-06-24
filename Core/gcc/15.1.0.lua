a8659bc672e5bd1b058009157b6abd41ab9b2f83
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-06-24 10:23:41 -0500

    openmpi under gcc/15/13

--- Core/gcc/15.1.0.lua
+++ Core/gcc/15.1.0.lua
@@ -8,5 +8,7 @@ setenv( "TACC_CC", "gcc" )
 setenv( "TACC_CXX", "g++" )
 setenv( "TACC_FC", "gfortran" )
 
-prepend_path("MODULEPATH", pathJoin(MP_ROOT, "Compiler",compiler,version))
+prepend_path( "MODULEPATH", pathJoin(MP_ROOT, "Compiler",compiler,"13.2.0") )
+prepend_path( "MODULEPATH", pathJoin(MP_ROOT, "Compiler",compiler,version) )
+
 family("compiler")
